7c44ae8085283a02a0c097ea93407c0384ef51c9
Cyprien G <36187005+neekkro@users.noreply.github.com>
2021-03-29 17:28:42 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBiiCLFcDoD-n2njnGm",
+    apiKey: "API",
     authDomain: "nzt-chat.firebaseapp.com",
     databaseURL: "https://nzt-chat.firebaseio.com",
     projectId: "nzt-chat",
     storageBucket: "nzt-chat.appspot.com",
     messagingSenderId: "202170904966",
-    appId: "1:202170904966:web:28aa7c865a469fc07sadadadada"
+    appId: "SOME"
     };
